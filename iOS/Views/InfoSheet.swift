3db0c638f536fbeb360bb06f2f27921cf04476be
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2022-02-15 20:32:32 -0500

    Revert changes to Xcode project files

--- iOS/Views/InfoSheet.swift
+++ iOS/Views/InfoSheet.swift
@@ -29,19 +29,15 @@ struct InfoSheet: View {
 						.font(.headline)
 					Text("Shuttle Tracker sends your location data to our server only when you tap “Board Bus” and stops sending these data when you tap “Leave Bus”. Your location data are associated with an anonymous, random identifier that rotates every time you start a new shuttle trip. These data aren’t associated with your name, Apple ID, RCS ID, or any other identifying information. We continuously purge location data that are more than 30 seconds old from our server. We may retain resolved location data that are calculated using a combination of system- and user-reported data indefinitely, but these resolved data don’t correspond with any specific user-reported coordinates.")
 				}
+					.padding(.horizontal)
 			}
-				.padding(.horizontal)
 				.navigationTitle("Shuttle Tracker 🚐")
 				.toolbar {
 					ToolbarItem {
 						CloseButton()
 					}
 				}
-        }
-        .padding(18.0)
-        
-        
-        
+		}
 	}
 	
 }
