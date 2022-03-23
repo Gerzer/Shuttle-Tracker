a4505c8473cbc880a709ae58ce947677a09da2a0
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2022-03-23 00:42:56 -0400

    Add menu item to see what’s new on macOS

--- macOS/ShuttleTrackerApp.swift
+++ macOS/ShuttleTrackerApp.swift
@@ -57,6 +57,15 @@ import OnboardingKit
 					}
 						.keyboardShortcut(KeyEquivalent("a"), modifiers: [.command, .shift])
 						.disabled(self.sheetStack.count > 0 && self.sheetStack.top != .announcements)
+					Button("\(self.sheetStack.top == .whatsNew ? "Hide" : "Show") What’s New") {
+						if self.sheetStack.top == .whatsNew {
+							self.sheetStack.pop()
+						} else {
+							self.sheetStack.push(.whatsNew)
+						}
+					}
+						.keyboardShortcut(KeyEquivalent("w"), modifiers: [.command, .shift])
+						.disabled(self.sheetStack.count > 0 && self.sheetStack.top != .whatsNew)
 					Divider()
 					Button("Re-Center Map") {
 						self.mapState.mapView?.setVisibleMapRect(MapUtilities.mapRect, animated: true)
