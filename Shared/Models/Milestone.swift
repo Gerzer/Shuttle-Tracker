cadc4dc4168d8e691712ba0f2c1d2feb8156f39e
Truong Tommy <truongtommy1999@gmail.com>
2022-04-05 17:09:21 -0400

    Displaying each milestones

--- Shared/Models/Milestone.swift
+++ Shared/Models/Milestone.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Milestone:Codable {
+struct Milestone:Codable,Hashable {
     var id:UUID
     var name:String
     var extendedDescription : String
@@ -15,7 +15,31 @@ struct Milestone:Codable {
     var progressType:String
     var goals:[Int]
     var signature:Data
+}
+
+class MilestonesViewModel : ObservableObject {
+    @Published var milestones = [Milestone]()
     
+    func fetch() {
+        guard let url = URL(string: "https://shuttletracker.app/milestones") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data,_,error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let milestones = try JSONDecoder().decode([Milestone].self, from: data)
+                DispatchQueue.main.async {
+                    self?.milestones = milestones
+                }
+            }catch {
+                print(error)
+            }
+
+        }
+        task.resume()
+    }
 }
 
 
--- Shared/Views/MilestoneToastView.swift
+++ Shared/Views/MilestoneToastView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 struct MilestoneToastView: View {
+    
+    @StateObject var viewModel = MilestonesViewModel()
     @EnvironmentObject private var viewState: ViewState
+    
     
     @State private var numberOfBoard:Int = 95
     @State private var levels = [100,200,300,400,500]
@@ -17,9 +20,75 @@ struct MilestoneToastView: View {
     @State var milestones = [Milestone]()
         
     var body: some View {
+        ScrollView {
+            
+            HStack{
+            Text("Milestones")
+                    .bold()
+                    .font(.largeTitle)
+                    .padding()
+                Spacer()
+            }
+        VStack{
+                ForEach(viewModel.milestones,id: \.self) { milestone in
+                    var r = Double.random(in: 0...1)
+                    var g = Double.random(in: 0...1)
+                    var b = Double.random(in: 0...1)
+                
+                    MilestoneToast(){
+                        withAnimation {
+                            self.viewState.toastType = nil
+                        }
+                    }  content : {
+                        HStack {
+                            Image(systemName: "skew")
+                                .resizable()
+                                .frame(width: 26, height: 24)
+                            Text(milestone.name)
+                                .font(.system(size: 32, weight: .medium, design: .default))
+                        }
+                        .foregroundColor(Color(red: r, green: g, blue: b))
+                        HStack{
+                            Text(milestone.extendedDescription)
+                                .font(.system(size: 18, weight: .bold, design: .default))
+                        }
+                        Divider()
+                        Spacer()
+                            .frame(height: 5)
+                        
+                        HStack(alignment: .lastTextBaseline){
+                            Text("\(milestone.progress)")
+                                .bold()
+                                .font(.largeTitle)
+                            Text("out of 200 rides")
+                        }
+                        Spacer()
+                            .frame(height: 5)
+                        ProgressBar(progressValue: 0.3)
+                        
+                        HStack(alignment: .lastTextBaseline){
+                            Text("199")
+                                .font(.largeTitle)
+                                .bold()
+                            Text("out of 10 stages")
+                        }
+                        Spacer()
+                            .frame(height: 5)
+                        ProgressBar(progressValue: 0.7)
+                        
+                    }
+                    .padding()
+                }
+        }
+        }
+            .onAppear {
+                viewModel.fetch()
+            }
         
+        /*
         ScrollView {
             VStack{
+                
                 HStack{
                 Text("Milestones")
                         .bold()
@@ -46,6 +115,7 @@ struct MilestoneToastView: View {
                         .font(.system(size: 18, weight: .bold, design: .default))
                 }
                 Divider()
+         
                 HStack(alignment: .lastTextBaseline){
                     Text(self.milestones[0].extendedDescription)
                         .bold()
@@ -76,7 +146,6 @@ struct MilestoneToastView: View {
                 HStack{
                 Image(systemName: "bus")
                         .foregroundColor(.red)
-                    
                 Text("Why Shuttle Tracker")
                         .bold()
                         .font(.title)
@@ -84,7 +153,6 @@ struct MilestoneToastView: View {
                     }
                 Divider()
                 Text("Because")
-                
             }
             .padding()
                 
@@ -95,23 +163,9 @@ struct MilestoneToastView: View {
         }
 
     }
+         
     
-    
-    func loadData() async {
-        guard let url = URL(string: "https://shuttletracker.app/milestones")
-        else {
-            print("RIP milestones")
-            return
-        }
-        do {
-            let (data,_) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode([Milestone].self, from: data){
-                milestones = decodedResponse
-            }
-        } catch {
-            print("INVALID DATA")
-        }
-    }
+
     
 
     var currentLevel:Int {
@@ -129,6 +183,8 @@ struct MilestoneToastView: View {
             let res1 = Double(self.numberOfBoard)/Double(levels[currentLevel]) //progress in the current stage
             let res2 = Double(currentLevel)/Double(self.levels.count) // progress in total of all the stages.
             return (res1,res2)
+         
+        */
     
     }
 }
