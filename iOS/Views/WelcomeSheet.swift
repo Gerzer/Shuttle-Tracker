5d312a77f64a824a5c9993e6409da4a46d1584e5
emmaleengo <73451814+emmaleengo@users.noreply.github.com>
2022-04-24 15:50:59 -0400

    Updated Welcomesheet.swift
    Changed some grammar and added in styling to match other sheets

--- iOS/Views/WelcomeSheet.swift
+++ iOS/Views/WelcomeSheet.swift
@@ -26,8 +26,10 @@ struct WelcomeSheet: View {
 			.padding(.bottom)
 			Text("Shuttle Tracker shows you the real-time locations of the RPI campus shuttles, powered by crowd-sourced location data.")
 				.padding(.bottom)
-			Text("Privacy")
-				.font(.headline)
+            Divider()
+			Text("PRIVACY")
+                .font(.system(size: 16, weight: .light, design: .default))
+                .padding(.top)
 			Text("Shuttle Tracker sends your location data to our server only when you tap “Board Bus” and stops sending these data when you tap “Leave Bus”. Your location data are associated with an anonymous, random identifier that rotates every time you start a new shuttle trip. These data aren’t associated with your name, Apple ID, RCS ID, or any other identifying information. We continuously purge location data that are more than 30 seconds old from our server. We may retain resolved location data that are calculated using a combination of system- and user-reported data indefinitely, but these resolved data don’t correspond with any specific user-reported coordinates.")
 			Spacer()
 			Button {
