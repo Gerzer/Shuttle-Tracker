1985bcb9a8bdcd26f0f22c8b4ac747987c636ed1
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2022-10-18 17:29:51 -0400

    added refined icons to shuttle tracker network entry sheet

--- iOS/Views/NetworkEntrySheet.swift
+++ iOS/Views/NetworkEntrySheet.swift
@@ -15,15 +15,68 @@ struct NetworkEntrySheet: View {
     
     @EnvironmentObject private var viewState: ViewState
     
+    @State private var notificationAuthorizationStatus: UNAuthorizationStatus?
+    
+    @State private var locationScale: CGFloat = 0
+    
+    @State private var notificationScale: CGFloat = 0
+    
     var body: some View {
         SheetPresentationWrapper{
             NavigationView{
                 ScrollView{
-                    VStack(alignment: .leading, spacing: 0 ){
-                        
+                    VStack(alignment: .leading, spacing: 25 ){
+                        Group{
+                            HStack {
+                                Image(systemName: "bus")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.accentColor)
+                                
+                                Text("Get live location data of busses 24/7!")
+                            }
+                            
+                            
+                            HStack {
+                                Image(systemName: "figure.walk.circle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.accentColor)
+                                    
+                                Text("Automaticlly board busses in close proximity!")
+                            }
+                            
+                            HStack {
+                                Image(systemName: "network")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.accentColor)
+                                Text("A more refined user experience!")
+                            }
+                            
+                            
+                            HStack{
+                                
+                                Text("The shuttle tracker network ")
+                            }
+                            
+                            
+                            
+                        }    .scaleEffect(self.notificationScale)
+                            .onAppear {
+                                withAnimation(.easeIn(duration: 0.5).delay(0.5)) {
+                                    self.notificationScale = 1.3
+                                }
+                                withAnimation(.easeOut(duration: 0.2).delay(1)) {
+                                    self.notificationScale = 1
+                                }
+                            }
                     }
                 }
-                .navigationTitle("Shuttle Tracker 🚐")
+                .navigationTitle("Shuttle Tracker Network")
                 .toolbar {
                     ToolbarItem {
                         CloseButton()
