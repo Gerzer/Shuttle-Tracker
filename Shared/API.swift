9183e555b413762573abf14bc88fc735b964a1de
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2022-10-21 11:38:23 -0400

    Improve code style

--- Shared/API.swift
+++ Shared/API.swift
@@ -22,13 +22,13 @@ enum API: TargetType {
 	
 	case readAllBuses
 	
-	case readBus(_ id: Int)
+	case readBus(id: Int)
 	
-	case updateBus(_ id: Int, location: Bus.Location)
+	case updateBus(id: Int, location: Bus.Location)
 	
-	case boardBus(_ id: Int)
+	case boardBus(id: Int)
 	
-	case leaveBus(_ id: Int)
+	case leaveBus(id: Int)
 	
 	case readRoutes
 	
