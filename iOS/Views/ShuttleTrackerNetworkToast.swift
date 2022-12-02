28be0ef953cbd870804f7cfb2653a821d0135120
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2022-12-01 23:18:23 -0500

    Shuttle tracker network, toast if location is off.

---
+++ iOS/Views/ShuttleTrackerNetworkToast.swift
@@ -0,0 +1,27 @@
+//
+//  ShuttleTrackerNetworkToast.swift
+//  iOS
+//
+//  Created by John Foster on 12/1/22.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct ShuttleTrackerNetworkToast: View {
+    
+    @EnvironmentObject private var viewState: ViewState
+    
+    var body: some View {
+        Toast("We cant see you!") {
+            withAnimation {
+                self.viewState.toastType = nil
+            }
+        } content: {
+            Text("Enable location services for optimal usage of the Shuttle Tracker Network.")
+        }
+    }
+    
+}
+
