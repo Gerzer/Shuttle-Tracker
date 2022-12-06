180c3496927d9628fa8097d7084ca086c328c32d
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2022-12-06 18:44:00 -0500

    Added button Logic to toast

--- iOS/Views/ShuttleTrackerNetworkToast.swift
+++ iOS/Views/ShuttleTrackerNetworkToast.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ShuttleTrackerNetworkToast: View {
 
     @EnvironmentObject private var viewState: ViewState
+    @Environment(\.openURL) private var openURL
+    @EnvironmentObject private var sheetStack: SheetStack
 
     var body: some View {
         Toast("We cant see you!") {
@@ -22,6 +24,27 @@ struct ShuttleTrackerNetworkToast: View {
             
             VStack{
                 Button{
+                    
+                    switch (LocationUtilities.locationManager.authorizationStatus, LocationUtilities.locationManager.accuracyAuthorization) {
+                                           case (.authorizedAlways, .fullAccuracy), (.authorizedWhenInUse, .fullAccuracy):
+                                              break
+                                           case (.restricted, _), (.denied, _):
+                                               let url = try! UIApplication.openSettingsURLString.asURL()
+                                               self.openURL(url)
+                                           case (.notDetermined, _):
+                                               LocationUtilities.locationManager.requestAlwaysAuthorization()
+                                           case (_, .reducedAccuracy):
+                                               Task {
+                                                   do {
+                                                       try await LocationUtilities.locationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "BoardBus")
+                                                   } catch let error {
+                                                       print("[Network_toast body] Full-accuracy location authorization request error: \(error.localizedDescription)")
+                                                   }
+                                               }
+                                           @unknown default:
+                                               fatalError()
+                                           }
+                                           self.sheetStack.pop()
                     
                 }label: {
                     
