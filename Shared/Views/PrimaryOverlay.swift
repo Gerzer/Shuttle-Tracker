5ca829055f75d5cb24764930e2ef2a11acf49a42
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2022-12-07 13:39:55 -0500

    Fix behavior of the “Board Bus” button when the user’s location is unavailable

--- Shared/Views/PrimaryOverlay.swift
+++ Shared/Views/PrimaryOverlay.swift
@@ -44,118 +44,122 @@ struct PrimaryOverlay: View {
 		.autoconnect()
 	
 	var body: some View {
-		HStack {
-			Spacer()
-			VStack(alignment: .leading) {
-				Button {
-					Task {
-						switch await self.boardBusManager.travelState {
-						case .onBus:
-							Logging.withLogger(for: .boardBus) { (logger) in
-								logger.log(level: .info, "[\(#fileID):\(#line) \(#function, privacy: .public)] “Leave Bus” button tapped")
-							}
-							await self.boardBusManager.leaveBus()
-							self.viewState.statusText = .thanks
-							LocationUtilities.locationManager.stopUpdatingLocation()
-							
-							// Remove any pending leave-bus notifications
-							UNUserNotificationCenter
-								.current()
-								.removeAllPendingNotificationRequests()
-							
-							// Request a review on the App Store
-							// This logic uses the legacy SKStoreReviewController class because the newer SwiftUI requestReview environment value requires iOS 16 or newer, and stored properties can’t be gated on OS version.
-							// TODO: Switch to SwiftUI’s requestReview environment value when we drop support for iOS 15
-							let windowScenes = UIApplication.shared.connectedScenes
-								.filter { (scene) in
-									return scene.activationState == .foregroundActive
+		SheetPresentationWrapper {
+			HStack {
+				Spacer()
+				VStack(alignment: .leading) {
+					Button {
+						Task {
+							switch await self.boardBusManager.travelState {
+							case .onBus:
+								Logging.withLogger(for: .boardBus) { (logger) in
+									logger.log(level: .info, "[\(#fileID):\(#line) \(#function, privacy: .public)] “Leave Bus” button tapped")
 								}
-								.compactMap { (scene) in
-									return scene as? UIWindowScene
+								await self.boardBusManager.leaveBus()
+								self.viewState.statusText = .thanks
+								LocationUtilities.locationManager.stopUpdatingLocation()
+								
+								// Remove all pending leave-bus notifications
+								UNUserNotificationCenter
+									.current()
+									.removeAllPendingNotificationRequests()
+								
+								// Request a review on the App Store
+								// This logic uses the legacy SKStoreReviewController class because the newer SwiftUI requestReview environment value requires iOS 16 or newer, and stored properties can’t be gated on OS version.
+								// TODO: Switch to SwiftUI’s requestReview environment value when we drop support for iOS 15
+								let windowScenes = UIApplication.shared.connectedScenes
+									.filter { (scene) in
+										return scene.activationState == .foregroundActive
+									}
+									.compactMap { (scene) in
+										return scene as? UIWindowScene
+									}
+								if let windowScene = windowScenes.first {
+									SKStoreReviewController.requestReview(in: windowScene)
 								}
-							if let windowScene = windowScenes.first {
-								SKStoreReviewController.requestReview(in: windowScene)
-							}
-							
-							do {
-								if #available(iOS 16, *) {
-									try await Task.sleep(for: .seconds(5))
+								
+								do {
+									if #available(iOS 16, *) {
+										try await Task.sleep(for: .seconds(5))
+									} else {
+										try await Task.sleep(nanoseconds: 5_000_000_000)
+									}
+								} catch let error {
+									Logging.withLogger(doUpload: true) { (logger) in
+										logger.log(level: .error, "[\(#fileID):\(#line) \(#function, privacy: .public)] Task sleep error: \(error, privacy: .public)")
+									}
+									throw error
+								}
+								self.viewState.statusText = .mapRefresh
+							case .notOnBus:
+								Logging.withLogger(for: .boardBus) { (logger) in
+									logger.log(level: .info, "[\(#fileID):\(#line) \(#function, privacy: .public)] “Board Bus” button tapped")
+								}
+								guard let userLocation = LocationUtilities.locationManager.location else {
+									self.sheetStack.push(.permissions)
+									Logging.withLogger(for: .location) { (logger) in
+										logger.log("[\(#fileID):\(#line) \(#function, privacy: .public)] User location is unavailable")
+									}
+									break
+								}
+								let closestStopDistance = await self.mapState.stops.reduce(into: .greatestFiniteMagnitude) { (distance, stop) in
+									let newDistance = stop.location.distance(from: userLocation)
+									if newDistance < distance {
+										distance = newDistance
+									}
+								}
+								if closestStopDistance < Double(self.appStorageManager.maximumStopDistance) {
+									self.sheetStack.push(.busSelection)
+									if self.viewState.toastType == .boardBus {
+										self.viewState.toastType = nil
+									}
 								} else {
-									try await Task.sleep(nanoseconds: 5_000_000_000)
-								}
-							} catch let error {
-								Logging.withLogger(doUpload: true) { (logger) in
-									logger.log(level: .error, "[\(#fileID):\(#line) \(#function, privacy: .public)] Task sleep error: \(error, privacy: .public)")
-								}
-								throw error
-							}
-							self.viewState.statusText = .mapRefresh
-						case .notOnBus:
-							Logging.withLogger(for: .boardBus) { (logger) in
-								logger.log(level: .info, "[\(#fileID):\(#line) \(#function, privacy: .public)] “Board Bus” button tapped")
-							}
-							
-							// TODO: Rename local `location` identifier to something more descriptive
-							guard let location = LocationUtilities.locationManager.location else {
-								break
-							}
-							let closestStopDistance = await self.mapState.stops.reduce(into: Double.greatestFiniteMagnitude) { (distance, stop) in
-								let newDistance = stop.location.distance(from: location)
-								if newDistance < distance {
-									distance = newDistance
+									self.viewState.alertType = .noNearbyStop
 								}
 							}
-							if closestStopDistance < Double(self.appStorageManager.maximumStopDistance) {
-								self.sheetStack.push(.busSelection)
-								if self.viewState.toastType == .boardBus {
-									self.viewState.toastType = nil
-								}
+						}
+					} label: {
+						Text(self.buttonText)
+							.bold()
+					}
+						.buttonStyle(.block)
+					HStack {
+						Text(self.viewState.statusText.rawValue)
+							.layoutPriority(1)
+						Spacer()
+						Group {
+							if self.isRefreshing {
+								ProgressView()
 							} else {
-								self.viewState.alertType = .noNearbyStop
-							}
-						}
-					}
-				} label: {
-					Text(self.buttonText)
-						.bold()
-				}
-					.buttonStyle(.block)
-				HStack {
-					Text(self.viewState.statusText.rawValue)
-						.layoutPriority(1)
-					Spacer()
-					Group {
-						if self.isRefreshing {
-							ProgressView()
-						} else {
-							Button {
-								if CalendarUtilities.isAprilFools {
-									self.sheetStack.push(.plus(featureText: "Refreshing the map"))
-								} else {
-									NotificationCenter.default.post(name: .refreshBuses, object: nil)
+								Button {
+									if CalendarUtilities.isAprilFools {
+										self.sheetStack.push(.plus(featureText: "Refreshing the map"))
+									} else {
+										NotificationCenter.default.post(name: .refreshBuses, object: nil)
+									}
+								} label: {
+									Image(systemName: "arrow.clockwise")
+										.resizable()
+										.aspectRatio(1, contentMode: .fit)
+										.symbolVariant(.circle)
+										.symbolVariant(.fill)
+										.symbolRenderingMode(.multicolor)
 								}
-							} label: {
-								Image(systemName: "arrow.clockwise")
-									.resizable()
-									.aspectRatio(1, contentMode: .fit)
-									.symbolVariant(.circle)
-									.symbolVariant(.fill)
-									.symbolRenderingMode(.multicolor)
 							}
 						}
+							.frame(width: 30)
 					}
-						.frame(width: 30)
 				}
+					.padding()
+					.background(.regularMaterial)
+					.mask {
+						RoundedRectangle(cornerRadius: 20, style: .continuous)
+					}
+					.shadow(radius: 5)
+				Spacer()
 			}
 				.padding()
-				.background(.regularMaterial)
-				.mask {
-					RoundedRectangle(cornerRadius: 20, style: .continuous)
-				}
-				.shadow(radius: 5)
-			Spacer()
 		}
-			.padding()
 			.onReceive(NotificationCenter.default.publisher(for: .refreshBuses)) { (_) in
 				withAnimation {
 					self.isRefreshing = true
