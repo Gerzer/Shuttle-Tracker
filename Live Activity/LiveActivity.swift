3b4c7d60a1fd619f348a72651028138285013e25
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2023-01-27 17:57:57 -0500

    Add unfinished Live Activity

---
+++ Live Activity/LiveActivity.swift
@@ -0,0 +1,84 @@
+//
+//  LiveActivity.swift
+//  Live Activity
+//
+//  Created by Gabriel Jacoby-Cooper on 1/27/23.
+//
+
+import ActivityKit
+import SwiftUI
+import WidgetKit
+
+struct LiveActivityAttributes: ActivityAttributes {
+	
+	public struct ContentState: Codable, Hashable {
+		// Dynamic stateful properties about your activity go here!
+		var value: Int
+	}
+	
+	// Fixed non-changing properties about your activity go here!
+	var name: String
+	
+}
+
+struct LiveActivity: Widget {
+	
+	var body: some WidgetConfiguration {
+		ActivityConfiguration(for: LiveActivityAttributes.self) { (context) in
+			// Lock screen/banner UI goes here
+			VStack {
+				Text("Hello")
+			}
+				.activityBackgroundTint(Color.cyan)
+				.activitySystemActionForegroundColor(Color.black)
+			
+		} dynamicIsland: { (context) in
+			DynamicIsland {
+				// Expanded UI goes here.  Compose the expanded UI through
+				// various regions, like leading/trailing/center/bottom
+				DynamicIslandExpandedRegion(.leading) {
+					Text("Leading")
+				}
+				DynamicIslandExpandedRegion(.trailing) {
+					Text("Trailing")
+				}
+				DynamicIslandExpandedRegion(.bottom) {
+					Text("Bottom")
+					// more content
+				}
+			} compactLeading: {
+				Text("L")
+			} compactTrailing: {
+				Text("T")
+			} minimal: {
+				Text("Min")
+			}
+				.widgetURL(URL(string: "http://www.apple.com"))
+				.keylineTint(Color.red)
+		}
+	}
+	
+}
+
+struct LiveActivityPreviews: PreviewProvider {
+	
+	static let attributes = LiveActivityAttributes(name: "Me")
+	
+	static let contentState = LiveActivityAttributes.ContentState(value: 3)
+	
+	static var previews: some View {
+		self.attributes
+			.previewContext(self.contentState, viewKind: .dynamicIsland(.compact))
+			.previewDisplayName("Island Compact")
+		self.attributes
+			.previewContext(self.contentState, viewKind: .dynamicIsland(.expanded))
+			.previewDisplayName("Island Expanded")
+		self.attributes
+			.previewContext(self.contentState, viewKind: .dynamicIsland(.minimal))
+			.previewDisplayName("Minimal")
+		self.attributes
+			.previewContext(self.contentState, viewKind: .content)
+			.previewDisplayName("Notification")
+	}
+	
+}
---
+++ Live Activity/LiveActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  LiveActivityBundle.swift
+//  Live Activity
+//
+//  Created by Gabriel Jacoby-Cooper on 1/27/23.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct LiveActivityBundle: WidgetBundle {
+	
+	var body: some Widget {
+		LiveActivity()
+	}
+	
+}
