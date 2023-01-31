e8b2ec0874636c82c87334c29efc248ae5245fe6
buddyskian <ianianian@fastmail.com>
2023-01-31 15:41:02 -0500

    Added the file for the View code for the shuttle network page

---
+++ Shared/Views/ShuttleNetworkView.swift
@@ -0,0 +1,21 @@
+//
+//  ShuttleNetworkView.swift
+//  Shuttle Tracker
+//
+//  Created by Ian Evans on 1/31/23.
+//
+//Will hold the code for the view that pushes users to turn on always on notifications
+
+import SwiftUI
+
+struct AnnoyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnnoyView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnnoyView()
+    }
+}
