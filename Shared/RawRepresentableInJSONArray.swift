8667d1ff4a8f9f80d48a53b5f6705e48a38c9a9c
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2023-08-23 17:55:31 -0400

    Remove unsafe code in `RawRepresentable`

--- Shared/RawRepresentableInJSONArray.swift
+++ Shared/RawRepresentableInJSONArray.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// We use a dedicated protocol instead of just extending Array for all Element specializations that conform to Codable to avoid creating retroactive conformaces.
+
 public protocol RawRepresentableInJSONArray: Codable { }
 
 extension Array: RawRepresentable where Element: RawRepresentableInJSONArray {
@@ -14,8 +16,9 @@ extension Array: RawRepresentable where Element: RawRepresentableInJSONArray {
 	public var rawValue: String {
 		get {
 			// Serialize this array into a single JSON string
-			let data = try! JSONEncoder().encode(self) // FIXME: Handle error condition gracefully
-			return String(data: data, encoding: .utf8)! // FIXME: Remove force-unwrap
+			return (try? JSONEncoder().encode(self)).flatMap { (data) in
+				return String(data: data, encoding: .utf8)
+			} ?? "[ ]"
 		}
 	}
 	
