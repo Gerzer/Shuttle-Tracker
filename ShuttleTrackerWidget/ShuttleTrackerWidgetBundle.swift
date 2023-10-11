f84eb04c1aa096bc6020dba7923e6bdf11e6e215
Truong Tommy <79810374+TruongTommy99@users.noreply.github.com>
2023-10-11 15:42:25 -0400

    added initial widget files

---
+++ ShuttleTrackerWidget/ShuttleTrackerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ShuttleTrackerWidgetBundle.swift
+//  ShuttleTrackerWidget
+//
+//  Created by Tommy Truong on 10/3/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ShuttleTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ShuttleTrackerWidget()
+    }
+}
