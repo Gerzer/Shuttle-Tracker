29eef050b62a71a31cacb2af14360cc9eab4835b
yic04 <144494441+yic04@users.noreply.github.com>
2023-12-08 23:35:36 -0500

    Implementing Widgets Bus Stops

---
+++ Stops-Widget/Stops_WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Stops_WidgetBundle.swift
+//  Stops-Widget
+//
+//  Created by Yi Chen on 12/8/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Stops_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Stops_Widget()
+        Stops_WidgetLiveActivity()
+    }
+}
